f3440bd8c897d57f345e849faa826f5222d53bdb
Gary Wenneker <gary.wenneker@iodigital.com>
2024-12-16 21:58:23 +0100

    Refactor install script by commenting out SSH setup code for clarity

--- src/io/install.sh
+++ src/io/install.sh
@@ -19,70 +19,70 @@ cat > /usr/local/bin/ssh-setup \
 << EOF
 #!/bin/bash
 
-COL='\033[1;33m'
-WHITE='\033[1;37m'
+# COL='\033[1;33m'
+# WHITE='\033[1;37m'
 
 
-eval "$(ssh-agent -s)"
+# eval "$(ssh-agent -s)"
 
-clear
+# clear
 
-echo -e "${COL}
-                 %%%%%                        %%%%%                        
-                 %%%%%%%%               %%%%%%%%%%%%%%%%                   
-                 %%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%                
-                 %%%%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%              
-                  %%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%             
-                   %%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-                    %%%%        %%%%%%%%%%%          %%%%%%%%%%%           
-                      %%%       %%%%%%%%%%             %%%%%%%%%%          
-                     %%%%%%    %%%%%%%%%%              %%%%%%%%%%          
-                    %%%%%%%%   %%%%%%%%%                %%%%%%%%%          
-                   %%%%%%%%%%  %%%%%%%%%                %%%%%%%%%          
-                  %%%%%%%%%%%   %%%%%%%%%              %%%%%%%%%%          
-                  %%%%%%%%%%%   %%%%%%%%%%            %%%%%%%%%%           
-                 %%%%%%%%%%%     %%%%%%%%%%%%%    %%%%%%%%%%%%%%           
-                %%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-               %%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%              
-               %%%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%%%               
-                 %%%%%%%%             %%%%%%%%%%%%%%%%%%%%                 
-                      %%                  %%%%%%%%%%%%                     
-${WHITE}"
-echo -n "Please enter key name: "
-read key
-printf "Please enter your email-adress:"
-read email
-
-
-ssh-keygen -t rsa -b 8192 -C "$email" -f ~/.ssh/$key
-
-clear
-
-FILE=~/.ssh/config
-if [ -f "$FILE" ]; then
-	# add line to the config file
-	echo "Host *
-	AddKeysToAgent yes
-	UseKeychain yes
-	IdentityFile ~/.ssh/$key" >> ~/.ssh/config
-else 
-	printf "$FILE created and $key added to ssh store for persistency"
-	echo "Host *
-	AddKeysToAgent yes
-	UseKeychain yes
-	IdentityFile ~/.ssh/$key" > ~/.ssh/config
-fi
-
-printf "$key Key added to $FILE for persistency.\n\n"
-
-printf "Please add this public key to your git account or in bridge:\n\n"
-echo "-----BEGIN SSH2 PUBLIC KEY-----"
-cat ~/.ssh/$key.pub
-echo "-----END SSH2 PUBLIC KEY-----"
-printf "\n\n"
+# echo -e "${COL}
+#                  %%%%%                        %%%%%                        
+#                  %%%%%%%%               %%%%%%%%%%%%%%%%                   
+#                  %%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%                
+#                  %%%%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%              
+#                   %%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%             
+#                    %%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+#                     %%%%        %%%%%%%%%%%          %%%%%%%%%%%           
+#                       %%%       %%%%%%%%%%             %%%%%%%%%%          
+#                      %%%%%%    %%%%%%%%%%              %%%%%%%%%%          
+#                     %%%%%%%%   %%%%%%%%%                %%%%%%%%%          
+#                    %%%%%%%%%%  %%%%%%%%%                %%%%%%%%%          
+#                   %%%%%%%%%%%   %%%%%%%%%              %%%%%%%%%%          
+#                   %%%%%%%%%%%   %%%%%%%%%%            %%%%%%%%%%           
+#                  %%%%%%%%%%%     %%%%%%%%%%%%%    %%%%%%%%%%%%%%           
+#                 %%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+#                %%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%              
+#                %%%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%%%               
+#                  %%%%%%%%             %%%%%%%%%%%%%%%%%%%%                 
+#                       %%                  %%%%%%%%%%%%                     
+# ${WHITE}"
+# echo -n "Please enter key name: "
+# read key
+# printf "Please enter your email-adress:"
+# read email
 
 
-ssh-add ~/.ssh/$key
+# ssh-keygen -t rsa -b 8192 -C "$email" -f ~/.ssh/$key
+
+# clear
+
+# FILE=~/.ssh/config
+# if [ -f "$FILE" ]; then
+# 	# add line to the config file
+# 	echo "Host *
+# 	AddKeysToAgent yes
+# 	UseKeychain yes
+# 	IdentityFile ~/.ssh/$key" >> ~/.ssh/config
+# else 
+# 	printf "$FILE created and $key added to ssh store for persistency"
+# 	echo "Host *
+# 	AddKeysToAgent yes
+# 	UseKeychain yes
+# 	IdentityFile ~/.ssh/$key" > ~/.ssh/config
+# fi
+
+# printf "$key Key added to $FILE for persistency.\n\n"
+
+# printf "Please add this public key to your git account or in bridge:\n\n"
+# echo "-----BEGIN SSH2 PUBLIC KEY-----"
+# cat ~/.ssh/$key.pub
+# echo "-----END SSH2 PUBLIC KEY-----"
+# printf "\n\n"
+
+
+# ssh-add ~/.ssh/$key
 
 EOF
 
