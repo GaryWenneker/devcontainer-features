bab9377a075fbc407edf5efaa4b716ec8f7cd9bd
Gary Wenneker <gary.wenneker@iodigital.com>
2024-12-17 08:13:24 +0100

    Add iO feature with installation script and tests

--- src/io/install.sh
+++ src/io/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'iO'"
+echo "Activating feature 'io'"
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 #
@@ -15,75 +15,12 @@ echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_
 echo "The effective dev container containerUser is '$_CONTAINER_USER'"
 echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
 
-cat > /usr/local/bin/ssh-setup \
+cat > /usr/local/bin/io \
 << EOF
-#!/bin/bash
-
-# COL='\033[1;33m'
-# WHITE='\033[1;37m'
-
-
-# eval "$(ssh-agent -s)"
-
-# clear
-
-# echo -e "${COL}
-#                  %%%%%                        %%%%%                        
-#                  %%%%%%%%               %%%%%%%%%%%%%%%%                   
-#                  %%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%                
-#                  %%%%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%              
-#                   %%%%%%%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%             
-#                    %%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-#                     %%%%        %%%%%%%%%%%          %%%%%%%%%%%           
-#                       %%%       %%%%%%%%%%             %%%%%%%%%%          
-#                      %%%%%%    %%%%%%%%%%              %%%%%%%%%%          
-#                     %%%%%%%%   %%%%%%%%%                %%%%%%%%%          
-#                    %%%%%%%%%%  %%%%%%%%%                %%%%%%%%%          
-#                   %%%%%%%%%%%   %%%%%%%%%              %%%%%%%%%%          
-#                   %%%%%%%%%%%   %%%%%%%%%%            %%%%%%%%%%           
-#                  %%%%%%%%%%%     %%%%%%%%%%%%%    %%%%%%%%%%%%%%           
-#                 %%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-#                %%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%              
-#                %%%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%%%               
-#                  %%%%%%%%             %%%%%%%%%%%%%%%%%%%%                 
-#                       %%                  %%%%%%%%%%%%                     
-# ${WHITE}"
-# echo -n "Please enter key name: "
-# read key
-# printf "Please enter your email-adress:"
-# read email
-
-
-# ssh-keygen -t rsa -b 8192 -C "$email" -f ~/.ssh/$key
-
-# clear
-
-# FILE=~/.ssh/config
-# if [ -f "$FILE" ]; then
-# 	# add line to the config file
-# 	echo "Host *
-# 	AddKeysToAgent yes
-# 	UseKeychain yes
-# 	IdentityFile ~/.ssh/$key" >> ~/.ssh/config
-# else 
-# 	printf "$FILE created and $key added to ssh store for persistency"
-# 	echo "Host *
-# 	AddKeysToAgent yes
-# 	UseKeychain yes
-# 	IdentityFile ~/.ssh/$key" > ~/.ssh/config
-# fi
-
-# printf "$key Key added to $FILE for persistency.\n\n"
-
-# printf "Please add this public key to your git account or in bridge:\n\n"
-# echo "-----BEGIN SSH2 PUBLIC KEY-----"
-# cat ~/.ssh/$key.pub
-# echo "-----END SSH2 PUBLIC KEY-----"
-# printf "\n\n"
-
-
-# ssh-add ~/.ssh/$key
-
+#!/bin/sh
+RED='\033[0;91m'
+NC='\033[0m' # No Color
+echo "\${RED}iO!\${NC}"
 EOF
 
-chmod +x /usr/local/bin/ssh-setup
+chmod +x /usr/local/bin/io
